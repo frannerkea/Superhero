0eb61c5faee9688e4bef6431edb37f70d87fbd11
frannerkea <chri268u@stud.kea.dk>
2022-09-14 09:28:34 +0200

    delopgave 2 færdig

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,101 +10,76 @@ public class Main {
     int valg;
     Database database = new Database();
 
-   public void startMenu() {
-       System.out.println("Velkommen til the SUPERHERO PROGRAM");
-       System.out.println("1: Opret ny superhero");
-       System.out.println("2: Liste menu");
-       System.out.println("9: Afslut programet");
-       int valg = brugerInput.nextInt();
-       brugerInput.nextLine();
-       if (valg == 1) {
-           opretSuperhero();
-       }
-       else if (valg == 9) {
-           System.exit(0);
-       }
-       else if (valg == 2) {
-           listeMenu();
-       }
+    public void startMenu() {
+        int valg;
+        do {
+            System.out.println("Velkommen til the SUPERHERO PROGRAM");
+            System.out.println("1: Opret ny superhero");
+            System.out.println("2: Liste menu");
+            System.out.println("9: Afslut programet");
+            valg = brugerInput.nextInt();
+            brugerInput.nextLine();
+            if (valg == 1) {
+                opretSuperhero();
+            } else if (valg == 9) {
+                System.exit(0);
+            } else if (valg == 2) {
+                listeMenu();
+            }
+        } while (valg != 9);
 
 
+    }
 
-
-
-
-   }
-
-   public void listeMenu() {
-       System.out.println("liste af superheros");
-        for (Superhero helt : database.getSuperheroes() ) {
-            System.out.println(helt.getSuperHelteNavn() + helt.getSuperKraft() + helt.getVirkeligeNavn() +  helt.getOprindelsesår()  + helt.getErMenneske() + helt.getStyrke() + " " );
+    public void listeMenu() {
+        System.out.println("liste af superheros");
+        for (Superhero helt : database.getSuperheroes()) {
+            System.out.println(helt.getSuperHelteNavn() + helt.getSuperKraft() + helt.getVirkeligeNavn() + helt.getOprindelsesår() + helt.getErMenneske() + helt.getStyrke() + " ");
         }
 
-   }
+    }
 
-   public void opretSuperhero() {
-
-       //create superhero
-       Database database = new Database();
+    public void opretSuperhero() {
 
 
-
-       //text til brugeren og brugerInput
-
-
-           //SuperhelteNavn
-           System.out.println("Hvad er din superhelts superhelte navn?");
-           System.out.print("Skriv det her: ");
-           String superHelteNavn = brugerInput.nextLine();
-
-           //Superkraft
-           System.out.println("hvad er din superhelts superkraf?");
-           System.out.print("Skriv det her: ");
-           String superKraft = brugerInput.nextLine();
-
-           //virkeligeNavn
-           System.out.println("hvad er din superhelts virkelige navn?");
-           System.out.print("Skriv det her: ");
-           String virkeligeNavn = brugerInput.nextLine();
-
-           // oprindelsesår
-           System.out.println("Hvad er din superhelts oprindelesår?");
-           System.out.print("Skriv det her: ");
-           int oprindelsesår = brugerInput.nextInt();
-           brugerInput.nextLine();
-
-           // erMenneske
-           System.out.println("Er din superhelt menneske? ja eller nej");
-           System.out.println("skriv ja eller nej ");
-           String erMenneske = brugerInput.nextLine();
-
-           //styrke
-           System.out.println("Hvad er din superhelts styrke?");
-           System.out.print("Skriv lige tal mellem 1 og 10000 her: ");
-           double styrke = brugerInput.nextInt();
-
-           database.createSuperhero(superHelteNavn, superKraft, virkeligeNavn, oprindelsesår, erMenneske, styrke);
-
-       System.out.println("DU har oprettet in superhero. Hvad vil du nu?");
-       System.out.println("1: Opret ny superhero");
-       System.out.println("2: Liste menu");
-       System.out.println("9: Afslut programet");
-       int valg = brugerInput.nextInt();
-       brugerInput.nextLine();
-       if (valg == 1) {
-           opretSuperhero();
-       }
-       else if (valg == 9) {
-           System.exit(0);
-       }
-       else if (valg == 2) {
-           listeMenu();
-       }
-
-       }
+        //text til brugeren og brugerInput
 
 
+        //SuperhelteNavn
+        System.out.println("Hvad er din superhelts superhelte navn?");
+        System.out.print("Skriv det her: ");
+        String superHelteNavn = brugerInput.nextLine();
 
+        //Superkraft
+        System.out.println("hvad er din superhelts superkraf?");
+        System.out.print("Skriv det her: ");
+        String superKraft = brugerInput.nextLine();
+
+        //virkeligeNavn
+        System.out.println("hvad er din superhelts virkelige navn?");
+        System.out.print("Skriv det her: ");
+        String virkeligeNavn = brugerInput.nextLine();
+
+        // oprindelsesår
+        System.out.println("Hvad er din superhelts oprindelesår?");
+        System.out.print("Skriv det her: ");
+        int oprindelsesår = brugerInput.nextInt();
+        brugerInput.nextLine();
+
+        // erMenneske
+        System.out.println("Er din superhelt menneske? ja eller nej");
+        System.out.println("skriv ja eller nej ");
+        String erMenneske = brugerInput.nextLine();
+
+        //styrke
+        System.out.println("Hvad er din superhelts styrke?");
+        System.out.print("Skriv lige tal mellem 1 og 10000 her: ");
+        double styrke = brugerInput.nextInt();
+
+        database.createSuperhero(superHelteNavn, superKraft, virkeligeNavn, oprindelsesår, erMenneske, styrke);
+
+
+    }
 
 
     public static void main(String[] args) {
@@ -129,7 +104,6 @@ public class Main {
 
          */
     }
-
 
 
 }
